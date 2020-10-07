6f466529c4f622cd7ea6c04871b13276c3d7a852
Pavel Yakimenko <pavel.yakimenko@mongodb.com>
2020-10-07 17:39:02 +0100

    Should be '@objc'

--- Realm/RLMObjectSchema.mm
+++ Realm/RLMObjectSchema.mm
@@ -174,7 +174,7 @@ using namespace realm;
     }
 
     if ([objectClass shouldIncludeInDefaultSchema] && schema.isSwiftClass && schema.properties.count == 0) {
-        @throw RLMException(@"No properties are defined for '%@'. Did you remember to mark them with '@obj' in your model?", schema.className);
+        @throw RLMException(@"No properties are defined for '%@'. Did you remember to mark them with '@objc' in your model?", schema.className);
     }
     return schema;
 }
--- Realm/Tests/Swift/SwiftSchemaTests.swift
+++ Realm/Tests/Swift/SwiftSchemaTests.swift
@@ -119,7 +119,7 @@ class SwiftRLMSchemaTests: RLMMultiProcessTestCase {
 
     func testShouldRaiseObjectWithoutProperties() {
         assertThrowsWithReasonMatching(RLMObjectSchema(forObjectClass: NoProps.self),
-                                       "No properties are defined for 'NoProps'. Did you remember to mark them with '@obj' in your model?")
+                                       "No properties are defined for 'NoProps'. Did you remember to mark them with '@objc' in your model?")
     }
     
     func testSchemaInitWithLinkedToObjectUsingInitWithValue() {
